ad4697f95dde0e6ef55d8af01482a78f783050c1
Tiphaine giron <tiphaine.giron@gmail.com>
2024-02-16 11:45:54 +0100

    add resources

--- Package.swift
+++ Package.swift
@@ -7,16 +7,25 @@ let package = Package(
     name: "ZoomTEST",
     platforms: [.iOS(.v12)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ZoomSDK",
-            targets: ["ZoomSDK"])
+            targets: ["ZoomSDKTarget"])
     ],
     dependencies: [
     ],
     targets: [
+        .target(
+          name: "ZoomSDKTarget",
+          dependencies: [
+            "ZoomSDKBinaryTarget",
+          ],
+          path: "Sources",
+          exclude: [
+            "Resources"
+          ]
+        ),
         .binaryTarget(
-            name: "ZoomSDK",
+            name: "ZoomSDKBinaryTarget",
             url: "https://github.com/tiphainegiron/binary-package-test/releases/download/1.0.0/MobileRTC.xcframework.zip",
             checksum: "c3aa8d5b2896ad0a3460010eadc029f483a81660e69bdd2d0ad04924f43c088a"
         )
