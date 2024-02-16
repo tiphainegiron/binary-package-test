d34847d7360f32b352fcdf0eb80a459150fac19f
Tiphaine Giron <121811403+tiphainegiron@users.noreply.github.com>
2024-02-16 13:14:46 +0100

    minimalist Package.swift

--- Package.swift
+++ Package.swift
@@ -15,26 +15,10 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .target(
-          name: "ZoomTestTarget",
-          dependencies: [
-            "ZoomTestAll",
-            "_ZoomTest",
-          ],
-          path: "Sources/ZoomTest",
-          exclude: [
-            "Resources"
-          ]
-        ),
-        .target(
-          name: "ZoomTestAll",
-          publicHeadersPath: "./"
-        ),
         .binaryTarget(
-            name: "_ZoomTest",
-            url: "https://github.com/tiphainegiron/binary-package-test/releases/download/4.0.0/MobileRTC.xcframework.zip",
+            name: "ZoomTestTarget",
+            url: "https://github.com/tiphainegiron/binary-package-test/releases/download/5.0.0/MobileRTC.xcframework.zip",
             checksum: "c3aa8d5b2896ad0a3460010eadc029f483a81660e69bdd2d0ad04924f43c088a"
-           // checksum: "935aa00c585581cbf6f309545a2ef14d170e8f7e"
         )
     ]
 )
